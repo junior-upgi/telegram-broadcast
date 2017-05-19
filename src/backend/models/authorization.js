51b975ad0f14bd0e7fe1405e194858c3099a84f9
Chia-Yu Tsai <chiayu.tsai.personal@gmail.com>
2017-05-19 08:36:21 +0800

    package.json added a new package for password encryption
    models - started a SQLite database for user authentication purpose

---
+++ src/backend/models/authorization.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize, DataTypes) => {
+    const Authorizations = sequelize.define('authorizations', {
+        loginId: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            primaryKey: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
+        deletedAt: {
+            type: DataTypes.DATE,
+            allowNull: true
+        }
+    }, {
+        name: {
+            singular: 'authorization',
+            plural: 'authorizations'
+        }
+    });
+    return Authorizations;
+};
